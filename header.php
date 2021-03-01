0e977abcd3fb4126692f0cb69dcfca336823d123
TranDacLam <tdlam2105@gmail.com>
2021-03-01 23:45:58 +0700

    #3

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="icon" href="<?php echo THEME_URL ?>/favicon.ico">
+    <link rel="icon" href="<?php echo THEME_URL ?>/favicon.png">
     <!--    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;500;700&display=swap" rel="stylesheet">-->
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600&display=swap" rel="stylesheet">
